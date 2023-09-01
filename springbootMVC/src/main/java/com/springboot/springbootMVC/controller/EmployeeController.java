a723898c20d6a805be6649131d8ab8a7c37d2105
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2023-09-01 19:11:29 +0300

    Added add and save methoods to EmployeeController,created employee-form.html

--- springbootMVC/src/main/java/com/springboot/springbootMVC/controller/EmployeeController.java
+++ springbootMVC/src/main/java/com/springboot/springbootMVC/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -27,4 +29,25 @@ public class EmployeeController {
 
         return "list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee",theEmployee);
+
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        employeeService.save(theEmployee);
+
+        return "redirect:/";
+    }
+
+
+
 }
