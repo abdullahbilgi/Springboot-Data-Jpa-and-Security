60608209200ea698aed11e08aeab83b3d5c5acba
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2023-09-02 15:45:42 +0300

    created custom security login page

--- springbootMVC/src/main/java/com/springboot/springbootMVC/configuration/SecurityConfig.java
+++ springbootMVC/src/main/java/com/springboot/springbootMVC/configuration/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.springboot.springbootMVC.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -22,6 +24,26 @@ public class SecurityConfig {
                 "select user_id,role from roles where user_id=?");
 
         return jdbcUserDetailsManager;
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configuer ->
+                configuer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+
+        return http.build();
 
     }
 
